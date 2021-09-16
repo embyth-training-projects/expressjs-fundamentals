5720984631d03c206ecf545c0677307e79a39bfe
Rostyslav Miniukov <thresh1337@gmail.com>
2021-09-17 00:35:41 +0300

    Clearing out behavior in pre hook when calling the deleteOne function

--- models/product.js
+++ models/product.js
@@ -26,14 +26,16 @@ const productSchema = new Schema({
   },
 });
 
-productSchema.pre("deleteOne", function () {
-  const productId = this.getQuery()["_id"];
+productSchema.pre("deleteOne", { document: false, query: true }, function () {
+  // Calling Model.deleteOne({ name: 'test' }) returns query
+  // Calling Model.findOne().then(doc => doc.deleteOne()) returns document
+  const query = this.getQuery(); // returns query passed on Model.deleteOne(query) / exmpl: Product.deleteOne({ _id: productId, userId: req.user._id }) -> {_id: value, userId: value}
 
-  return User.find({ "cart.items.productId": productId })
+  return User.find({ "cart.items.productId": query._id })
     .then((users) =>
       users.map((user) => {
         const updatedCartItems = user.cart.items.filter(
-          (item) => item.productId.toString() !== productId
+          (item) => item.productId.toString() !== query._id
         );
         user.cart.items = updatedCartItems;
         return user.save();
