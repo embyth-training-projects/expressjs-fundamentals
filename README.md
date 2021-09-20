8d1945348e0b91e40a26ea5a8910c6663c7db34a
Rostyslav Miniukov <thresh1337@gmail.com>
2021-09-20 21:15:47 +0300

    Solution to fifteenth assignment for pagination

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # Assignment
 
-1. Handle file upload with multer
-
-2. Store file statically
-
-3. Save data to database
-
-4. Download file with authentication
-
-5. Stream pdf files using pdfkit
-
-6. Handle deleting files
+1. Add pagination to index and catalog page
--- controllers/admin.js
+++ controllers/admin.js
@@ -6,13 +6,32 @@ const Product = require("../models/product");
 
 const { deleteFile } = require("../helpers/utils");
 
+const ITEMS_PER_PAGE = 1;
+
 exports.getProducts = (req, res, next) => {
-  Product.find({ userId: req.user._id })
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+
+      return Product.find({ userId: req.user._id })
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render("admin/products", {
         prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
