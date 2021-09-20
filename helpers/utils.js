88115bc9fe5dd76d50ed85dd1b4d97ca48841e7e
Rostyslav Miniukov <thresh1337@gmail.com>
2021-09-20 20:20:39 +0300

    Solution to fourteenth assignment for file upload/download

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 
 const getProjectDirectory = () => path.dirname(require.main.filename);
@@ -5,5 +6,14 @@ const getProjectDirectory = () => path.dirname(require.main.filename);
 const getCombinedPath = (file, ...paths) =>
   path.join(getProjectDirectory(), ...paths, file);
 
+const deleteFile = (filePath) => {
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+};
+
 exports.getProjectDirectory = getProjectDirectory;
 exports.getCombinedPath = getCombinedPath;
+exports.deleteFile = deleteFile;
--- routes/shop.js
+++ routes/shop.js
@@ -20,6 +20,8 @@ router.post("/cart-delete-item", isAuth, shopController.deleteCartProduct);
 
 router.get("/orders", isAuth, shopController.getOrders);
 
+router.get("/orders/:orderId", isAuth, shopController.getInvoice);
+
 router.post("/create-order", isAuth, shopController.postOrder);
 
 module.exports = router;
